daf0376f663e89129fa76155d20c62103b99e939
Robert <lucesolare23@outlook.it>
2022-02-20 13:31:38 +0100

    funzionamento dell'animazione finale + sito in inglese

--- public/javascripts/controltransactions.js
+++ public/javascripts/controltransactions.js
@@ -1,10 +1,134 @@
-var btn_causale = document.getElementById('btn-causale');
+
+var amount;
+var reach;
+var chooseReason;
+let finishAnimation = false;
+//logic part of the app
+
+var btn_confirm = document.getElementById('btn-confirm');
 var notvalidparameters = document.getElementById('notvalidparameters');
 
 const contractAddress = "0xf8e81D47203A594245E36C48e151709F0C19fBe8";
 let abi = {};
 
 web3 = new Web3(ethereum);
+
+var x = $(window).width() - 400;
+
+//function that wait wuntil the variable becomes true, used for the final animation of the donation
+const waitUntil = (condition) => {
+  return new Promise((resolve) => {
+      let interval = setInterval(() => {
+          if (!condition()) {
+              return
+          }
+
+          clearInterval(interval)
+          resolve()
+      }, 100)
+  })
+}
+
+
+$('.choose').css('display', 'none');
+
+$('.donate form').on("click", function(){
+  amount = $('input[name=amount]:checked', '#donAmount').val();
+  reach = amount * 22;
+  $('#confirm .amount').text("ETH" + amount);
+  $('#check span').text("ETH" + amount);
+  $('#confirm strong').text(reach + " voters");
+});
+
+$(".donate button").on("click", function(){
+  $(".donate").toggleClass("active");
+  if( $(".donate").is(".active") ) {
+    $("form").slideDown(450, "easeOutQuart");
+  } else {
+    $("form").slideUp(300, "easeInOutQuad");
+  }
+});
+
+$('.donate label').on("click", function(){
+  setTimeout(function() {
+    if (amount == "other"){
+      $("#custom").css("margin-left", x/2);
+$("#custom").css("margin-right", x/2);
+      $('body').addClass('custom');
+      $(".donate").hide("slide", { easing: "easeInQuart", direction: "left" }, 700, function(){
+        $("#custom").show("slide", { easing: "easeOutQuart", direction: "right" }, 700);
+      });
+    } else {
+      $('body').removeClass('custom');
+      $(".donate").hide("slide", { easing: "easeInQuart", direction: "left" }, 700, function(){
+        $(".choose").show("slide", { easing: "easeOutQuart", direction: "right" }, 700);
+      });
+    }
+  }, 150);
+});
+
+$('#custom .next').on("click", function(){
+  amount = $('input[name=custom-amount]', '#customDonation').val();
+  reach = amount * 22;
+  $('#confirm .amount').text("ETH " + amount);
+  $('#check span').text("ETH " + amount);
+  $('#confirm strong').text(reach + " voters");
+  $("#custom").hide("slide", { easing: "easeInQuart", direction: "left" }, 700, function(){
+    $(".choose").show("slide", { easing: "easeOutQuart", direction: "right" }, 700);
+  });
+});
+
+$('#custom .back').on("click", function(){
+  $("#custom").hide("slide", { easing: "easeInQuart", direction: "right" }, 700, function(){
+    $(".donate").show("slide", { easing: "easeOutQuart", direction: "left" }, 700);
+  });
+});
+
+
+$('.choose form').on("click", function(){
+  chooseReason = $('input[name=choose-reason]:checked', '#chooseReason').val();
+  $('#confirm .choose-reason').text("ETH " + amount);
+  $('#check span').text("ETH " + amount);
+  $('#confirm strong').text(reach + " voters");
+});
+
+$(".choose button").on("click", function(){
+  $(".choose").toggleClass("active");
+  if( $(".choose").is(".active") ) {
+    $("form").slideDown(450, "easeOutQuart");
+  } else {
+    $("form").slideUp(300, "easeInOutQuad");
+  }
+});
+
+$('.choose label').on("click", function(){
+  $(".choose ").hide("slide", { easing: "easeInQuart", direction: "left" }, 700, function(){
+    $("#check").show("slide", { easing: "easeOutQuart", direction: "left" }, 700);
+  });
+});
+
+$('#check .back').on("click", function(){
+  $("#check").hide("slide", { easing: "easeInQuart", direction: "right" }, 700, function(){
+    $(".choose").show("slide", { easing: "easeOutQuart", direction: "left" }, 700);
+  });
+});
+
+
+$("#check .next").on('click', async function(){
+  $("#check").hide("slide", { easing: "easeInQuart", direction: "left" }, 700, function(){
+    $(".processing").fadeIn(1500, function(){
+      $(".progress").animate({ width: "14em" }, 3500, "easeInOutCirc", function() {
+        $( ".mask" ).hide("slide", { easing: "easeInQuart", direction: "right" }, 400);
+      });
+    });
+  });
+
+  await waitUntil(() => finishAnimation == true);
+  setTimeout(function () { 
+    $('.processing .message, .outer').fadeOut();
+    $("#confirm").addClass('animated fadeInUp');
+  }, 4000);
+  });
 
 //function that check if the user is logged in
 function isConnected() {return Boolean(window.ethereum.selectedAddress !== null); }
@@ -77,22 +201,20 @@ function delCookie(cookiename, sPath, sDomain) {
                   (sPath ? "; path=" + sPath : "");
 }
 
-
 events();
 
 //function that init the transaction where you can send ethereum to the contract
 //choosing the reason for the donation the string will be passed in input to the function of the contract
 //and automatically the contract send the money to the wallet dedicated to the specific reason choiced
-btn_causale.addEventListener('click', async function() {
+btn_confirm.addEventListener('click', async function() {
     //get the reason for the donation and the amount of the donation
-    var reason = document.getElementById('sel_donator').value;
-    var quantityOfEthereum = document.getElementById('quantita').value.toString();
-
+    var reason = chooseReason;
+    var quantityOfEthereum = amount;
     //check for non valid parameter passed
-    if(reason === "Scegli" || quantityOfEthereum === "") notvalidparameters.innerText = "Non hai inserito alcuni parametri correttamente, riprova";
-    
-    else {
-        notvalidparameters.innerText = "";
+
+    if(quantityOfEthereum === "") alert("valore non corretto")
+
+    else{
         //converting the quantity in ethereum in wei and adding a "0x" to let understand metamask that i'm passing a hexadecimal number
         let quantityInWei = BigNumber(quantityOfEthereum).multipliedBy(10 ** 18);
 
@@ -106,11 +228,17 @@ btn_causale.addEventListener('click', async function() {
         var myContract = new web3.eth.Contract(abi, contractAddress);
 
         //sendig the transaction to the contract with the address of the person, the reason for the donation and the amount of the donation
-        myContract.methods.inviare(reason).send({
+       await myContract.methods.inviare(reason).send({
             from: fromAddress,
             to: contractAddress,
             value: "0x" + quantityInWei.toString(16),
             gas: "50000"
-        });
-    }   
+        },).then((response) => {
+            finishAnimation = true;            
+        })
+        .catch((err => {
+          console.log("errore");
+        }));
+        
+    }
 });
--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -5,7 +5,10 @@ var path = require('path');
 
 
 router.get('/getabi', (req, res) => {
-    res.sendFile(path.join(__dirname, '../contracts/Donazioni.json'));
+    res.sendFile(path.join(__dirname, '../contracts/Donations.json'));
 });
 
+router.get('/finishTransaction', (req, res) => {
+    res.send(true);
+});
 module.exports = router;
