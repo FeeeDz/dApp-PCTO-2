0780f746b9720f1a25b5c293771b1bdda60a4b20
Robert <lucesolare23@outlook.it>
2022-02-26 15:29:29 +0100

    dovrebbe funzionare, dimenticato di decremenentare la i nel ciclo invece che aumentarla

--- public/javascripts/dynamictable.js
+++ public/javascripts/dynamictable.js
@@ -10,7 +10,7 @@ async function buildHtmlTable(selector) {
     await fetchData();
     var columns = addAllColumnHeaders(dataForTable, selector);
   
-    for (var i = dataForTable.length; i >= 0 ; i++) {
+    for (var i = dataForTable.length; i >= 0 ; i--) {
       var row$ = $('<tr/>').addClass("hover:bg-gray-100 dark:hover:bg-gray-700");
       for (var colIndex = 0; colIndex < columns.length; colIndex++) {
         var cellValue = dataForTable[i][columns[colIndex]];
